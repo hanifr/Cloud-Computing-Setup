9808e2cbb52b866112820ed5a4683e10b80cc734
hanifr <irhaniframli@gmail.com>
2021-10-03 21:34:37 +0800

    Update nr_dependencies.sh

--- nr_dependencies.sh
+++ nr_dependencies.sh
@@ -5,8 +5,8 @@
 echo "${_GREEN}INSTALL DEPENDS STARTED${_RESET}"
     # Install NodeJS
     sudo apt-get update
-    #curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-    #sudo bash nodesource_setup.sh
+    curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+    sudo bash nodesource_setup.sh
 
     sudo apt-get upgrade -y
     sleep 5
@@ -14,8 +14,6 @@ echo "${_GREEN}INSTALL DEPENDS STARTED${_RESET}"
     sudo apt-get install npm
     
     #  Install Node-RED
-    sudo apt install build-essential git curl
-    bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --node14
     sudo npm install -g --unsafe-perm node-red node-red-admin
     sleep 5
 echo "${_CYAN} Now I will set open port 1880${_RESET}"
